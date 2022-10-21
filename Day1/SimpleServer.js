19210b7d0f9b28b5c2ef5cedf6b5013fe5f14ec7
chakradhar4 <chakradharghute@gmail.com>
2022-10-21 19:30:10 +0530

    Creating Simple Server in NOde

--- Day1/SimpleServer.js
+++ Day1/SimpleServer.js
@@ -1,20 +1,19 @@
-//simple server in node js 
-var http = require('http');
+//simple server in node js
+var http = require("http");
 
 //mention host here it is localhost
-var host ='127.0.0.1'
+var host = "127.0.0.1";
 //port adress
-var port = 3000
+var port = 3000;
 
-var server http.createServer((request, response) => {
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    console.log("server working");
-    response.end('Server Working Success');
+var server = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  console.log("server working");
+  response.end("Server Working Success");
 });
-server.listen(port,host,(error) => {
-    if(error){
-        return console.log('Error occured : ',error);
-    }
-    console.log('server is listening on '+host +":"+port);
+server.listen(port, host, (error) => {
+  if (error) {
+    return console.log("Error occured : ", error);
+  }
+  console.log("server is listening on " + host + ":" + port);
 });
-
