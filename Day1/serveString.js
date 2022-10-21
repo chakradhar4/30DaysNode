f85790390d90c3de591f335164c3ea8cc5a0eea7
chakradhar4 <chakradharghute@gmail.com>
2022-10-21 19:40:55 +0530

    serving String in server

---
+++ Day1/serveString.js
@@ -0,0 +1,17 @@
+var http = require("http");
+
+var host = "127.0.0.1";
+var port = 3000;
+
+var server = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  //we can send as many responses as we want using response.write() method.
+  reponse.write("Hello world");
+  reponse.end();
+});
+server.listen(port, host, (error) => {
+  if (error) {
+    return console.log("Error occured :", error);
+  }
+  console.log("server is listening on " + host + ":" + port);
+});
