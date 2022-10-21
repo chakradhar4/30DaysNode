95da64b7708092fddacf19b24a335a8f4a3349ac
chakradhar4 <chakradharghute@gmail.com>
2022-10-21 19:41:01 +0530

    Update SimpleServer.js

--- Day1/SimpleServer.js
+++ Day1/SimpleServer.js
@@ -7,8 +7,10 @@ var host = "127.0.0.1";
 var port = 3000;
 
 var server = http.createServer((request, response) => {
+  //response.write() : It is the inbuilt method which is used to send the response
   response.writeHead(200, { "Content-Type": "text/plain" });
   console.log("server working");
+  //we can only send one response using this
   response.end("Server Working Success");
 });
 server.listen(port, host, (error) => {
